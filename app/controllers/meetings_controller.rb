8393a0599a3afa85574fbc73fa825115bb696a87
Paul Shippy <paul.shippy@strongmind.com>
2025-08-13 21:32:52 -0700

    Fix paging view

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -2,20 +2,27 @@ class MeetingsController < ApplicationController
   include Pagy::Backend
 
   def index
-    @pagy, @meetings = pagy(Meeting.includes(:municipality).order(held_on: :desc), items: 20)
     @sort = params[:sort]
-    @direction = params[:direction] == 'asc' ? 'asc' : 'desc'
+    @direction = params[:direction] == "asc" ? "asc" : "desc"
+
+    meetings_query = Meeting.includes(:municipality)
 
     if @sort.present?
       case @sort
-      when 'date'
-        @meetings = @meetings.reorder(held_on: @direction)
-      when 'meeting_type'
-        @meetings = @meetings.reorder(meeting_type: @direction)
-      when 'municipality'
-        @meetings = @meetings.joins(:municipality).reorder('municipalities.name ' + @direction)
+      when "date"
+        meetings_query = meetings_query.order(held_on: @direction)
+      when "meeting_type"
+        meetings_query = meetings_query.order(meeting_type: @direction)
+      when "municipality"
+        meetings_query = meetings_query.joins(:municipality).order("municipalities.name #{@direction}")
+      else
+        meetings_query = meetings_query.order(held_on: :desc)
       end
+    else
+      meetings_query = meetings_query.order(held_on: :desc)
     end
+
+    @pagy, @meetings = pagy(meetings_query, items: 20)
   end
 
   def show
